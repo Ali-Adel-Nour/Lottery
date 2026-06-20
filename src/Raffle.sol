e6d495cbd8e74034533468d3abf3ce1f4a900942
Ali Nour Al Din <aliiiadel841@gmail.com>
2026-06-20 18:36:53 +0300

    feat:add custom errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -7,18 +7,40 @@ pragma solidity ^0.8.19;
  * @notice This contract is for creating a sample raffle
  */
 
+
 contract Raffle {
-    uint256 private  i_entranceFee;
+   /* Custom Erros*/
+
+    error Raffle_NotEnoughEth();
+
+    uint256 private immutable I_ENTRANCE_FEE;
 
     constructor(uint256 entranceFee) {
-        i_entranceFee = entranceFee;
+        I_ENTRANCE_FEE = entranceFee;
     }
 
-   function enterRaffle() public {
+   function enterRaffle() public payable {
+        //require(msg.value >= i_entranceFee, "Not enough ETH sent");
+
+         //more readable
+
+        //require(msg.value >= I_ENTRANCE_FEE, Raffle_NotEnoughEth());
+        
+        //Gas Effiecent
+        if (msg.value < I_ENTRANCE_FEE){
+            revert Raffle_NotEnoughEth();
+        }
+
+       
 
    }
 
    function pickWinner() public {
 
+   }
+
+   /* Getter Functions */
+   function getEntranceFee() external view returns (uint256) {
+        return I_ENTRANCE_FEE;
    }
 }
